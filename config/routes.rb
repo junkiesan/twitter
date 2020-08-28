6708a39d0f2eb99349aef194fdec69996bebc373
Julien Bouland <bouland.julien@gmail.com>
2020-08-28 15:36:29 +0200

    redirect to dashboard when signed_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "dashboards#show", as: :dashboard
+  end
+
   root to: "homes#show"
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
@@ -10,7 +13,7 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
 end
 
-get "/sign_in" => "sessions#new", as: "sign_in"
-delete "/sign_out" => "sessions#destroy", as: "sign_out"
-get "/sign_up" => "users#new", as: "sign_up"
+# get "/sign_in" => "sessions#new", as: "sign_in"
+# delete "/sign_out" => "sessions#destroy", as: "sign_out"
+# get "/sign_up" => "users#new", as: "sign_up"
 end
